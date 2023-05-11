c854b0e0b7497d008572587b1a7a2ef732b4cce9
MolotkovD <damolotkov05@gmail.com>
2023-05-11 23:00:06 +0300

    create base AC Sistem

---
+++ Ants/AppLogic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> points = TspFileReader.ReadTspFile(@"C:\Users\user\Downloads\Ant-Colony-System-master\TSP\kroA150.tsp");    // Parse TSPlib file and load as List<Point>
+
+            Graph graph = new Graph(points, true);  // Create Graph
+            GreedyAlgorithm greedyAlgorithm = new GreedyAlgorithm(graph);
+            double greedyShortestTourDistance = greedyAlgorithm.Run();  // get shortest tour using greedy algorithm
+
+            Parameters parameters = new Parameters(2, 0.1, 0.01, (1.0 / (graph.Dimensions * greedyShortestTourDistance)), 0.9, 20, 10000);
+            parameters.Show();
+
+            Solver solver = new Solver(parameters, graph);
+            List<double> results = solver.RunACS(); // Run ACS
+
+            Console.WriteLine("Time: " + solver.GetExecutionTime());
+            Console.ReadLine();
+        }
+    }
+}
